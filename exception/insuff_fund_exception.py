8dcc961f317101ff451f3d58d6c07a66522acbcf
Netra05 <108513484+Netra05@users.noreply.github.com>
2025-04-15 14:36:44 +0530

    Update insuff_fund_exception.py

--- exception/insuff_fund_exception.py
+++ exception/insuff_fund_exception.py
@@ -1,5 +1,5 @@
 class InsufficientFundsException(Exception):
-    def __init__(self, message="❌ Donation must be at least $10."):
+    def __init__(self, message=" Donation must be at least $10."):
         self.message = message
         super().__init__(self.message)
 
